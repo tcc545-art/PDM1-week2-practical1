e9a33b85950663f9affd04965ad6667a60c2c199
tcc545-art <tcc545@york.ac.uk>
2025-10-01 12:57:59 +0100

    shapes and mouse

---
+++ exercise1-1/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(600, 600);
+    
+}
+
+function draw() {
+    background(0);
+    fill(255)
+    rectMode
+    rect(0,0,mouseX,mouseY)
+
+    
+}
---
+++ exercise1-2/sketch.js
@@ -0,0 +1,26 @@
+function setup(){
+createCanvas(600,600);
+
+
+}
+
+
+function draw(){
+    background(255);
+
+fill(0,0,250,);
+rect(0,0, width / 2,height);
+//1st rect^^
+fill(150,0,200);
+rect(width/2,0,width / 2,height/2);
+//2th rect ^^
+fill(234)
+rect(width/2,width / 2,height/2);
+//3th rect ^^
+fill(0);
+rect(width*0.75,4,width / 2,height/2);
+//4th ^^
+fill(0,100,100,);
+rect(width*0.75,2,width / 4,height/2);
+
+}
